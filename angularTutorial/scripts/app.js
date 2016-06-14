d71f1afeb5b5d51ba05e7003203d13a459a46ad6
Rubén García Alba <Rubn Garca Alba>
2016-06-14 18:58:39 +0200

    Include of directives

---
+++ angularTutorial/scripts/app.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myApp", []);
+//Controllers
+app.controller("myCrtl", function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
+//Directives
+/*app.directive("directivetest", function () {
+    return {
+        template: "<h1>Made by a directive!</h1>"
+    };
+});*/
+
+var app = angular.module("myApp", []);
+app.directive("w3TestDirective", function () {
+    return {
+        template: "<h1>Made by a directive!</h1>"
+    };
+});
